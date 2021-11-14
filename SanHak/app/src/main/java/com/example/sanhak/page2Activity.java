6a0495100dff1c661d58b32aa662621a4adc7e73
woojin0415 <75172426+woojin0415@users.noreply.github.com>
2021-11-15 00:54:56 +0900

    Update : Spinner Setting
    스피너(셀렉트 박스) 설정 추가

--- SanHak/app/src/main/java/com/example/sanhak/page2Activity.java
+++ SanHak/app/src/main/java/com/example/sanhak/page2Activity.java
@@ -3,6 +3,10 @@ package com.example.sanhak;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +15,28 @@ public class page2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page2);
+
+        String[] Region = {"-지역 선택-","서울", "경기", "인천", "충청북도"};
+
+        Spinner sp1 = (Spinner) findViewById(R.id.region);
+        Spinner selectbox_detail = (Spinner) findViewById(R.id.region_detail);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,Region);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        sp1.setAdapter(adapter);
+
+        sp1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),"선택 실험",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
 
     }
 
