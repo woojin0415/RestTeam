a6e4f1cc316969f0033cc274ce6b611181367b49
woojin0415 <75172426+woojin0415@users.noreply.github.com>
2021-11-15 14:53:38 +0900

    Update : activity2page
    지역 세분화 및 두 번째 스피너 추가

--- SanHak/app/src/main/java/com/example/sanhak/page2Activity.java
+++ SanHak/app/src/main/java/com/example/sanhak/page2Activity.java
@@ -17,9 +17,26 @@ public class page2Activity extends AppCompatActivity {
         setContentView(R.layout.page2);
 
         String[] Region = {"-지역 선택-","서울", "경기", "인천", "충청북도"};
-
+        String[] Region_Seoul={"-세부 지역 선택-", "강서구", "양천구", "구로구","영등포구","금천구","관악구","동작구","서초구"
+        , "강남구", "송파구", "강동구", "흥신구","마포구","서대문구","종로구","중구","성동구","광진구","중랑구","동대문구","서대문구",
+        "은평구","성북구","강북구","도봉구","노원구"};
+        String[] Region_Chungbuk={"단양군","제천시","충주시","음성군","진천군","증평군","괴산군","청주시","보은군","옥천군","영동군"};
+        
+        
+        
+        
         Spinner sp1 = (Spinner) findViewById(R.id.region);
         Spinner selectbox_detail = (Spinner) findViewById(R.id.region_detail);
+        
+        
+        
+        ArrayAdapter<String> adapter_seoul = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,Region_Seoul);
+        adapter_seoul.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        ArrayAdapter<String> adapter_chungbuk = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,Region_Chungbuk);
+        adapter_chungbuk.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,Region);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
@@ -28,7 +45,12 @@ public class page2Activity extends AppCompatActivity {
         sp1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(),"선택 실험",Toast.LENGTH_SHORT).show();
+                if(sp1.getSelectedItem().toString() == "서울"){
+                    selectbox_detail.setAdapter(adapter_seoul);
+                }
+                else if(sp1.getSelectedItem().toString() == "충청북도"){
+                    selectbox_detail.setAdapter(adapter_chungbuk);
+                }
             }
 
             @Override
