373a3367070849b43cb25f56d9393e333b0e2ed9
woojin0415 <75172426+woojin0415@users.noreply.github.com>
2021-11-22 17:07:44 +0900

    Update page3Activity.java
    사용 확인된 API 코드 업데이터

--- SanHak/app/src/main/java/com/example/sanhak/page3Activity.java
+++ SanHak/app/src/main/java/com/example/sanhak/page3Activity.java
@@ -1,10 +1,13 @@
 package com.example.sanhak;
 
+import android.app.AppComponentFactory;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,72 +25,118 @@ public class page3Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page3);
 
+        String service_key = "TBK8Bq75wKqViN5AH0Hzxl25kH3AbT8G5ps96GKkAg%2Fw63QDxpysaPMLy8Gc3r4nOD3MCn%2Bn0dl0I2wON9ZwSQ%3D%3D";
+        String num_of_rows = "10";
+        String page_no = "1";
+        String data_type = "XML";
+        String stnId = "11B00000";
+        String tmFc = "202111211800";
+
+        String url = "http://apis.data.go.kr/1360000/MidFcstInfoService/getMidLandFcst?"+
+                "serviceKey="+service_key+
+                "&numOfRows="+num_of_rows+
+                "&pageNo="+page_no+
+                "&regId="+stnId+
+                "&tmFc="+tmFc;
+
         String region = (String) getIntent().getStringExtra("region");
 
+
+        NetworkTask networktask = new NetworkTask(url, null);
+        networktask.execute();
+
     }
-}
+    public class RequsetHttpConnection {
+        public String request(String _url, ContentValues _params) {
+            HttpURLConnection urlConn = null;
+            StringBuffer sbParmas = new StringBuffer();
 
-public class RequsetHttpConnection {
-    public String request(String _url, ContentValues _params) {
-        HttpURLConnection urlConn = null;
-        StringBuffer sbParmas = new StringBuffer();
+            if (_params == null)
+                sbParmas.append("");
+            else {
+                boolean isAnd = false;
 
-        if (_params == null)
-            sbParmas.append("");
-        else {
-            boolean isAnd = false;
+                String key;
+                String value;
 
-            String key;
-            String value;
+                for (Map.Entry<String, Object> parameter : _params.valueSet()) {
+                    key = parameter.getKey();
+                    value = parameter.getValue().toString();
 
-            for (Map.Entry<String, Object> parameter : _params.valueSet()) {
-                key = parameter.getKey();
-                value = parameter.getValue().toString();
+                    if (isAnd)
+                        sbParmas.append("&");
 
-                if (isAnd)
-                    sbParmas.append("&");
+                    sbParmas.append(key).append("=").append(value);
 
-                sbParmas.append(key).append("=").append(value);
-
-                if (!isAnd)
-                    if (_params.size() >= 2)
-                        isAnd = true;
+                    if (!isAnd)
+                        if (_params.size() >= 2)
+                            isAnd = true;
+                }
             }
+
+            try {
+                URL url = new URL(_url);
+                urlConn = (HttpURLConnection) url.openConnection();
+
+                urlConn.setRequestMethod("GET");
+                urlConn.setRequestProperty("Accept-Charset", "UTF-8");
+                urlConn.setDoOutput(false);
+
+                if (urlConn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    Log.d("TAG", "API 연결 요청 실패");
+                    return null;
+                }
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+
+                String line;
+                String page = "";
+
+                while ((line = reader.readLine()) != null){
+                    page += line;
+                }
+
+                return page;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally{
+                if(urlConn != null)
+                    urlConn.disconnect();
+            }
+
+            return null;
+        }
+    }
+
+
+    public class NetworkTask extends AsyncTask<Void, Void, String>{
+        private String url;
+        private ContentValues values;
+
+        public NetworkTask(String url, ContentValues values){
+            this.url = url;
+            this.values = values;
         }
 
-        try {
-            URL url = new URL(_url);
-            urlConn = (HttpURLConnection) url.openConnection();
 
-            urlConn.setRequestMethod("GET");
-            urlConn.setRequestProperty("Accept-Charset", "UTF-8");
-            urlConn.setDoOutput(false);
+        @Override
+        protected String doInBackground(Void... voids) {
 
-            if (urlConn.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                Log.d("TAG", "API 연결 요청 실패");
-                return null;
-            }
+            String result;
+            RequsetHttpConnection requestHttpConnection = new RequsetHttpConnection();
+            result = requestHttpConnection.request(url,values);
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
-
-            String line;
-            String page = "";
-
-            while ((line = reader.readLine()) != null){
-                page += line;
-            }
-            
-            return page;
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally{
-            if(urlConn != null)
-                urlConn.disconnect();
+            return result;
         }
 
+        @Override
+        protected  void onPostExecute(String s){
+            super.onPostExecute(s);
 
+            System.out.println(s);
+        }
     }
 }
