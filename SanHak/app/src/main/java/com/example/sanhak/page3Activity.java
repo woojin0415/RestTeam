96a4f0e8ed78a353fdd85ba009d5f53d8f309c59
woojin0415 <75172426+woojin0415@users.noreply.github.com>
2021-11-22 16:19:47 +0900

    Update page3Activity.java
    API 기능을 도울 HttpRequestConnection 클래스추가

--- SanHak/app/src/main/java/com/example/sanhak/page3Activity.java
+++ SanHak/app/src/main/java/com/example/sanhak/page3Activity.java
@@ -1,10 +1,20 @@
 package com.example.sanhak;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
 
 public class page3Activity extends AppCompatActivity {
     @Override
@@ -15,5 +25,69 @@ public class page3Activity extends AppCompatActivity {
         String region = (String) getIntent().getStringExtra("region");
 
     }
+}
 
+public class RequsetHttpConnection {
+    public String request(String _url, ContentValues _params) {
+        HttpURLConnection urlConn = null;
+        StringBuffer sbParmas = new StringBuffer();
+
+        if (_params == null)
+            sbParmas.append("");
+        else {
+            boolean isAnd = false;
+
+            String key;
+            String value;
+
+            for (Map.Entry<String, Object> parameter : _params.valueSet()) {
+                key = parameter.getKey();
+                value = parameter.getValue().toString();
+
+                if (isAnd)
+                    sbParmas.append("&");
+
+                sbParmas.append(key).append("=").append(value);
+
+                if (!isAnd)
+                    if (_params.size() >= 2)
+                        isAnd = true;
+            }
+        }
+
+        try {
+            URL url = new URL(_url);
+            urlConn = (HttpURLConnection) url.openConnection();
+
+            urlConn.setRequestMethod("GET");
+            urlConn.setRequestProperty("Accept-Charset", "UTF-8");
+            urlConn.setDoOutput(false);
+
+            if (urlConn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                Log.d("TAG", "API 연결 요청 실패");
+                return null;
+            }
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+
+            String line;
+            String page = "";
+
+            while ((line = reader.readLine()) != null){
+                page += line;
+            }
+            
+            return page;
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally{
+            if(urlConn != null)
+                urlConn.disconnect();
+        }
+
+
+    }
 }
