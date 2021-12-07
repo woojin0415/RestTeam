c953976a60a10722315b774359758e8cba64d238
woojin0415 <75172426+woojin0415@users.noreply.github.com>
2021-12-07 16:31:36 +0900

    Update Server.py
    서버가 데이터를 계속해서 받을 수 있도록 만듦

--- Server.py
+++ Server.py
@@ -6,17 +6,24 @@ port = 9999  # Arbitrary non-privileged port
 num = 0
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_sock.bind((host, port))
+while True:
+    server_sock.listen(1)
+    print("기다리는 중")
+    client_sock, addr = server_sock.accept()
+    print("연결 완료")
+    data = client_sock.recv(4096).decode()
+    if data == "": break
+    print(data)
+    count = 0
 
-server_sock.listen(1)
 
-client_sock, addr = server_sock.accept()
+    while True:
+        data = client_sock.recv(4096).decode()
+        print("receive data")
+        if data == "" : break
+        print(data)
 
-data = client_sock.recv(4096).decode()
-
-
-
-
-client_sock.close()
-
+    client_sock.close()
+    print("연결 종료")
 
 server_sock.close()
